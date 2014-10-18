4aee8e86c0da7882acb4ce5d84be8ac2442ef092
De. Mi. <me@denismilovanov.net>
2014-10-18 21:46:30 +0400

    User types not under git are shown

--- lib/models/Database.php
+++ lib/models/Database.php
@@ -30,6 +30,33 @@ class Database
                         n.oid = p.pronamespace
                     WHERE   n.nspname = ?w AND
                             p.prolang > 16  -- non-system languages (plpgsql)
+                    ORDER BY p.proname;
+            ",
+                $sSchema
+            );
+
+        } else if ($sObjectIndex == 'types') {
+
+            return self::$oDB->selectIndexedTable("
+                SELECT t.typname, t.typname AS file, '' AS hash
+                    FROM pg_catalog.pg_type t
+                    INNER JOIN pg_catalog.pg_namespace n
+                        ON n.oid = t.typnamespace
+                    WHERE   n.nspname = ?w AND
+                            -- look for composite ('c') types only:
+                            (--t.typrelid = 0 OR -- for non user types such as hstore
+                                (SELECT c.relkind = 'c'
+                                    FROM pg_catalog.pg_class c
+                                    WHERE c.oid = t.typrelid)
+                            ) AND
+                            -- exclude type[]
+                            NOT EXISTS(
+                                SELECT 1
+                                    FROM pg_catalog.pg_type el
+                                    WHERE   el.oid = t.typelem AND
+                                            el.typarray = t.oid
+                            )
+                    ORDER BY t.typname;
             ",
                 $sSchema
             );
